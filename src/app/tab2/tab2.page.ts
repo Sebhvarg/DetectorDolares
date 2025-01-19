46ee1191d941e919a3cc1f704058c1f78e2a5ef8
Electrokevo <kevinsantiagomejiaparra@gmail.com>
2025-01-19 14:41:03 -0500

    final changes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,19 +1,30 @@
 import {Component} from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonButton, IonLabel} from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonText,
+  IonButton,
+  IonLabel,
+  IonIcon, IonGrid
+} from '@ionic/angular/standalone';
 import {ExploreContainerComponent} from '../explore-container/explore-container.component';
 import {AccesibilidadService} from '../services/accesibilidad.service'
+import {addIcons} from 'ionicons';
+import {add, remove, refresh} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonText, IonButton, IonLabel,],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonLabel, IonIcon, IonGrid,],
   providers: []
 })
 export class Tab2Page {
 
   constructor(protected accesibilidadService: AccesibilidadService) {
-
+  addIcons({add, remove, refresh});
   }
 
 
